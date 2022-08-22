d280a0d6fe371cfb6790fbd8296f782675811f66
brionychiu <yuikuta0618@gmail.com>
2022-08-22 17:42:22 +0800

    finished side project : reaction timer

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+    <p>Reaction time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 200) {
+            this.rank = 'Ninga finger'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid reflexes'
+        } else if (this.score < 600) {
+            this.rank = 'little finger'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
